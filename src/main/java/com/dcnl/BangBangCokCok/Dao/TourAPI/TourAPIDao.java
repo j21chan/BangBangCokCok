cbe7d90a86b6bccdeed77a8af94c3bdbb2880623
jc <chan_j@naver.com>
2018-05-23 20:58:45 +0900

    add complete TourAPI Controller / add complete TourAPIDAO

--- src/main/java/com/dcnl/BangBangCokCok/Dao/TourAPI/TourAPIDao.java
+++ src/main/java/com/dcnl/BangBangCokCok/Dao/TourAPI/TourAPIDao.java
@@ -8,7 +8,15 @@ import java.util.Map;
 import com.dcnl.BangBangCokCok.TourAPI.JsonParser.AreaBasedListJsonParser;
 import com.dcnl.BangBangCokCok.TourAPI.JsonParser.AreaCodeJsonParser;
 import com.dcnl.BangBangCokCok.TourAPI.JsonParser.CategoryCodeJsonParser;
+import com.dcnl.BangBangCokCok.TourAPI.JsonParser.DetailCommonJsonParser;
+import com.dcnl.BangBangCokCok.TourAPI.JsonParser.DetailImageJsonParser;
+import com.dcnl.BangBangCokCok.TourAPI.JsonParser.DetailInfoJsonParser;
+import com.dcnl.BangBangCokCok.TourAPI.JsonParser.DetailIntroJsonParser;
 import com.dcnl.BangBangCokCok.TourAPI.JsonParser.JsonParser;
+import com.dcnl.BangBangCokCok.TourAPI.JsonParser.LocationBasedListJsonParser;
+import com.dcnl.BangBangCokCok.TourAPI.JsonParser.SearchFestivalJsonParser;
+import com.dcnl.BangBangCokCok.TourAPI.JsonParser.SearchKeywordJsonParser;
+import com.dcnl.BangBangCokCok.TourAPI.JsonParser.SearchStayJsonParser;
 import com.dcnl.BangBangCokCok.TourAPI.UrlBuilder.UrlBuilder;
 import com.dcnl.BangBangCokCok.TourAPI.UrlRequest.UrlRequest;
 import com.dcnl.BangBangCokCok.Utility.MyResponse;
@@ -94,6 +102,256 @@ public class TourAPIDao {
 			jsonString = urlRequest.urlRequest(urlBuilder.areaBasedListURLBuild(numOfRows, pageNo, arrange,
 																				contentTypeId, areaCode, sigunguCode,
 																				cat1, cat2, cat3));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// JSON 파싱
+		try {
+			list = jsonParser.parse(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 응답 객체 생성
+		response = myResponse.toResponse(list);
+		
+		return response;
+	}
+	
+	// 위치 기반 리스트 조회
+	public Map<String, Object> getLocationBasedList(String numOfRows, String pageNo, String arrange,
+													String contentTypeId, String mapX, String mapY, String radius) {
+		// json 파서 객체 생성, DTO담을 리스트 생성
+		jsonParser = new LocationBasedListJsonParser();
+		ArrayList<Object> list = new ArrayList<Object>();
+		String jsonString = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		// TourAPI에 json 요청
+		try {
+			jsonString = urlRequest.urlRequest(urlBuilder.locationBasedListURLBuild(numOfRows, pageNo, arrange,
+											   										contentTypeId, mapX, mapY, radius));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// JSON 파싱
+		try {
+			list = jsonParser.parse(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 응답 객체 생성
+		response = myResponse.toResponse(list);
+		
+		return response;
+	}
+	
+	// 키워드 검색 조회
+	public Map<String, Object> getSearchKeyword(String numOfRows, String pageNo, String arrange,
+												String contentTypeId, String keyword,
+												String areaCode, String sigunguCode,
+												String cat1, String cat2, String cat3) {
+		// json 파서 객체 생성, DTO담을 리스트 생성
+		jsonParser = new SearchKeywordJsonParser();
+		ArrayList<Object> list = new ArrayList<Object>();
+		String jsonString = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		// TourAPI에 json 요청
+		try {
+			jsonString = urlRequest.urlRequest(urlBuilder.searchKeywordURLBuild(numOfRows, pageNo, arrange,
+																				contentTypeId, keyword,
+																				areaCode, sigunguCode,
+																				cat1, cat2, cat3));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// JSON 파싱
+		try {
+			list = jsonParser.parse(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 응답 객체 생성
+		response = myResponse.toResponse(list);
+		
+		return response;
+	}
+	
+	// [행사/공연/축제] 날짜 조회
+	public Map<String, Object> getSearchFestival(String numOfRows, String pageNo, String arrange,
+										 		 String eventStartDate, String eventEndDate,
+												 String areaCode, String sigunguCode) {
+		// json 파서 객체 생성, DTO담을 리스트 생성
+		jsonParser = new SearchFestivalJsonParser();
+		ArrayList<Object> list = new ArrayList<Object>();
+		String jsonString = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		// TourAPI에 json 요청
+		try {
+			jsonString = urlRequest.urlRequest(urlBuilder.searchFestivalURLBuild(numOfRows, pageNo, arrange,
+																				 eventStartDate, eventEndDate,
+																				 areaCode, sigunguCode));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// JSON 파싱
+		try {
+			list = jsonParser.parse(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 응답 객체 생성
+		response = myResponse.toResponse(list);
+		
+		return response;
+	}
+	
+	
+	// [숙박] 베니키아,한옥,굿스테이 목록 조회
+	public Map<String, Object> getSearchStay(String numOfRows, String pageNo, String arrange,
+										 	 String hanOk, String benikia, String goodStay,
+											 String areaCode, String sigunguCode) {
+		// json 파서 객체 생성, DTO담을 리스트 생성
+		jsonParser = new SearchStayJsonParser();
+		ArrayList<Object> list = new ArrayList<Object>();
+		String jsonString = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		// TourAPI에 json 요청
+		try {
+			jsonString = urlRequest.urlRequest(urlBuilder.searchStayURLBuild(numOfRows, pageNo, arrange,
+																			 hanOk, benikia, goodStay,
+																			 areaCode, sigunguCode));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// JSON 파싱
+		try {
+			list = jsonParser.parse(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 응답 객체 생성
+		response = myResponse.toResponse(list);
+		
+		return response;
+	}
+	
+	// 상세정보1 - 공통정보 조회
+	public Map<String, Object> getDetailCommon(String contentId, String contentTypeId,
+											   String defaultYN, String firstImageYN,
+											   String areacodeYN, String catcodeYN,
+											   String addrinfoYN, String mapinfoYN, String overviewYN) {
+		// json 파서 객체 생성, DTO담을 리스트 생성
+		jsonParser = new DetailCommonJsonParser();
+		ArrayList<Object> list = new ArrayList<Object>();
+		String jsonString = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		// TourAPI에 json 요청
+		try {
+			jsonString = urlRequest.urlRequest(urlBuilder.detailCommonURLBuild(contentId, contentTypeId,
+																			   defaultYN, firstImageYN,
+																			   areacodeYN, catcodeYN, addrinfoYN,
+																			   mapinfoYN, overviewYN));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// JSON 파싱
+		try {
+			list = jsonParser.parse(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 응답 객체 생성
+		response = myResponse.toResponse(list);
+		
+		return response;
+	}
+	
+	// 상세정보2 - 소개정보 조회
+	public Map<String, Object> getDetailIntro(String contentId, String contentTypeId, String introYN) {
+		
+		// json 파서 객체 생성, DTO담을 리스트 생성
+		jsonParser = new DetailIntroJsonParser();
+		ArrayList<Object> list = new ArrayList<Object>();
+		String jsonString = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		// TourAPI에 json 요청
+		try {
+			jsonString = urlRequest.urlRequest(urlBuilder.detailIntroURLBuild(contentId, contentTypeId, introYN));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// JSON 파싱
+		try {
+			list = jsonParser.parse(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 응답 객체 생성
+		response = myResponse.toResponse(list);
+		
+		return response;
+	}
+	
+	// 상세정보3 - 반복정보 조회
+	public Map<String, Object> getDetailInfo(String contentId, String contentTypeId, String detailYN) {
+		
+		// json 파서 객체 생성, DTO담을 리스트 생성
+		jsonParser = new DetailInfoJsonParser();
+		ArrayList<Object> list = new ArrayList<Object>();
+		String jsonString = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		// TourAPI에 json 요청
+		try {
+			jsonString = urlRequest.urlRequest(urlBuilder.detailInfoURLBuild(contentId, contentTypeId, detailYN));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// JSON 파싱
+		try {
+			list = jsonParser.parse(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 응답 객체 생성
+		response = myResponse.toResponse(list);
+		
+		return response;
+	}
+	
+	// 상세정보4 - 이미지정보 조회
+	public Map<String, Object> getDetailImage(String contentId, String contentTypeId, String imageYN) {
+		
+		// json 파서 객체 생성, DTO담을 리스트 생성
+		jsonParser = new DetailImageJsonParser();
+		ArrayList<Object> list = new ArrayList<Object>();
+		String jsonString = null;
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		// TourAPI에 json 요청
+		try {
+			jsonString = urlRequest.urlRequest(urlBuilder.detailImageURLBuild(contentId, contentTypeId, imageYN));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/dcnl/BangBangCokCok/TourAPIController.java
+++ src/main/java/com/dcnl/BangBangCokCok/TourAPIController.java
@@ -43,7 +43,7 @@ public class TourAPIController {
 	public Map<String, Object> areaBasedList(@RequestParam("numOfRows") 	String numOfRows,
 											 @RequestParam("pageNo") 		String pageNo,
 											 @RequestParam("arrange") 		String arrange,
-											 @RequestParam("contenttypeid") String contenttypeid,
+											 @RequestParam("contentTypeId") String contentTypeId,
 											 @RequestParam("areaCode")		String areaCode,
 											 @RequestParam("sigunguCode") 	String sigunguCode,
 											 @RequestParam("cat1") 			String cat1,
@@ -51,11 +51,134 @@ public class TourAPIController {
 											 @RequestParam("cat3") 			String cat3) {
 		
 		Map<String, Object> response = dao.getAreaBasedList(numOfRows, pageNo, arrange,
-															contenttypeid, areaCode, sigunguCode,
+															contentTypeId, areaCode, sigunguCode,
 															cat1, cat2, cat3);
 		
 		return response;
 	}
 	
+	// 위치 기반 관광정보 조회
+	@RequestMapping(value = "/locationBasedList")
+	public Map<String, Object> locationBasedList(@RequestParam("numOfRows") 	String numOfRows,
+												 @RequestParam("pageNo") 		String pageNo,
+												 @RequestParam("arrange") 		String arrange,
+												 @RequestParam("contentTypeId") String contentTypeId,
+												 @RequestParam("mapX") 			String mapX,
+												 @RequestParam("mapY") 			String mapY,
+												 @RequestParam("radius") 		String radius) {
+		
+		Map<String, Object> response = dao.getLocationBasedList(numOfRows, pageNo, arrange,
+																contentTypeId, mapX, mapY, radius);
+		
+		return response;
+	}
 	
+	// 키워드 검색 조회
+	@RequestMapping(value = "/searchKeyword")
+	public Map<String, Object> searchKeyword(@RequestParam("numOfRows") 	String numOfRows,
+											 @RequestParam("pageNo") 		String pageNo,
+											 @RequestParam("arrange") 		String arrange,
+											 @RequestParam("contentTypeId") String contentTypeId,
+											 @RequestParam("keyword") 		String keyword,
+											 @RequestParam("areaCode") 		String areaCode,
+											 @RequestParam("sigunguCode") 	String sigunguCode,
+											 @RequestParam("cat1") 			String cat1,
+											 @RequestParam("cat2") 			String cat2,
+											 @RequestParam("cat3") 			String cat3) {
+		
+		Map<String, Object> response = dao.getSearchKeyword(numOfRows, pageNo, arrange,
+															contentTypeId, keyword,
+															areaCode, sigunguCode,
+															cat1, cat2, cat3);
+		
+		return response;
+	}
+	
+	// [행사/공연/축제] 날짜 조회
+	@RequestMapping(value = "/searchFestival")
+	public Map<String, Object> searchFestival(@RequestParam("numOfRows") 	String numOfRows,
+											  @RequestParam("pageNo") 		String pageNo,
+											  @RequestParam("arrange") 		String arrange,
+											  @RequestParam("eventStartDate") String eventStartDate,
+											  @RequestParam("eventEndDate") 		String eventEndDate,
+											  @RequestParam("areaCode") 		String areaCode,
+											  @RequestParam("sigunguCode") 	String sigunguCode) {
+		
+		Map<String, Object> response = dao.getSearchFestival(numOfRows, pageNo, arrange,
+															 eventStartDate, eventEndDate,
+															 areaCode, sigunguCode);
+		
+		return response;
+	}
+	
+	// [숙박] 베니키아,한옥,굿스테이 목록 조회
+	@RequestMapping(value = "/searchStay")
+	public Map<String, Object> searchStay(@RequestParam("numOfRows")   String numOfRows,
+										  @RequestParam("pageNo") 	   String pageNo,
+										  @RequestParam("arrange") 	   String arrange,
+										  @RequestParam("hanOk") 	   String hanOk,
+										  @RequestParam("benikia") 	   String benikia,
+										  @RequestParam("goodStay")    String goodStay,
+										  @RequestParam("areaCode")    String areaCode,
+										  @RequestParam("sigunguCode") String sigunguCode) {
+		
+		Map<String, Object> response = dao.getSearchStay(numOfRows, pageNo, arrange,
+														 hanOk, benikia, goodStay,
+														 areaCode, sigunguCode);
+		
+		return response;
+	}
+	
+	// 상세정보1 - 공통정보 조회
+	@RequestMapping(value = "/detailCommon")
+	public Map<String, Object> detailCommon(@RequestParam("contentId")     String contentId,
+											@RequestParam("contentTypeId") String contentTypeId,
+											@RequestParam("defaultYN") 	   String defaultYN,
+											@RequestParam("firstImageYN")  String firstImageYN,
+											@RequestParam("areacodeYN")    String areacodeYN,
+											@RequestParam("catcodeYN")     String catcodeYN,
+											@RequestParam("addrinfoYN")    String addrinfoYN,
+											@RequestParam("mapinfoYN") 	   String mapinfoYN,
+											@RequestParam("overviewYN")    String overviewYN) {
+		
+		Map<String, Object> response = dao.getDetailCommon(contentId, contentTypeId,
+														   defaultYN, firstImageYN,
+														   areacodeYN, catcodeYN,
+														   addrinfoYN, mapinfoYN, overviewYN);
+		
+		return response;
+	}
+	
+	// 상세정보2 - 소개정보 조회
+	@RequestMapping(value = "/detailIntro")
+	public Map<String, Object> detailIntro(@RequestParam("contentId")     String contentId,
+											@RequestParam("contentTypeId") String contentTypeId,
+											@RequestParam("introYN") 	   String introYN) {
+		
+		Map<String, Object> response = dao.getDetailIntro(contentId, contentTypeId, introYN);
+		
+		return response;
+	}
+	
+	// 상세정보3 - 반복정보 조회
+	@RequestMapping(value = "/detailInfo")
+	public Map<String, Object> detailInfo(@RequestParam("contentId")     String contentId,
+											@RequestParam("contentTypeId") String contentTypeId,
+											@RequestParam("detailYN") 	   String detailYN) {
+		
+		Map<String, Object> response = dao.getDetailInfo(contentId, contentTypeId, detailYN);
+		
+		return response;
+	}
+	
+	// 상세정보4 - 이미지정보 조회
+	@RequestMapping(value = "/detailImage")
+	public Map<String, Object> detailImage(@RequestParam("contentId")     String contentId,
+											@RequestParam("contentTypeId") String contentTypeId,
+											@RequestParam("imageYN") 	   String imageYN) {
+		
+		Map<String, Object> response = dao.getDetailImage(contentId, contentTypeId, imageYN);
+		
+		return response;
+	}
 }
